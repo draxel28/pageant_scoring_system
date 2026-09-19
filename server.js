779b17e230115473795d13af3fd35652c609c59c
John Paul Valdez <valconfi1212@gmail.com>
2026-09-20 07:37:54 +0800

    added deadair display

--- server.js
+++ server.js
@@ -585,6 +585,7 @@ server.listen(PORT, '0.0.0.0', () => {
       console.log(`   OBS overlay:     http://${n.address}:${PORT}/overlay.html`);
       console.log(`   Segment Display: http://${n.address}:${PORT}/segment-display.html`);
       console.log(`   Sponsors Display:http://${n.address}:${PORT}/sponsors-display.html`);
+      console.log(`   Dead Air Display:http://${n.address}:${PORT}/deadair-display.html`);
     }
   });
   console.log('=======================================\n');
